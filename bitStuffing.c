289f45fbfd17945b52510dfcbf9d6a21998f66f8
G Srinivasa Eswara Vara Prasad <105410227+varaprasadtarunkumar@users.noreply.github.com>
2023-05-14 11:52:50 +0530

    Update bitStuffing.c

--- bitStuffing.c
+++ bitStuffing.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
-int main(){
-  int arr[20],i,n,count=0;
-  printf("Enter the number of bits: \n");
-  scanf("%d",&n);
-  printf("Enter the data bits(only 0's and 1's are allowed) \n");
-  for(i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-  }
-  //printing after stuffing
-  for(i=0;i<n;i++){
-    if(arr[i]==1)
-      count++;
-    else
-      count=0;
-    //stuffing
-    if(count==5){
-    printf("0");
-    count=0;}
-  }
+#include <stdio.h>
+int main() {
+    int arr[20], i, n, count = 0;
+    printf("Enter the number of bits: \n");
+    scanf("%d", &n);
+    printf("Enter the data bits (only 0's and 1's are allowed): \n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    //printing after stuffing
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        if (arr[i] == 1)
+            count++;
+        else
+            count = 0;
+        //stuffing
+        if (count == 5) {
+            printf("0 ");
+            count = 0;
+        }
+    }
+    return 0;
 }
